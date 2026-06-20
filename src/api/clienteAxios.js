b764627da70a823d8d6e70256ad3e3a1b7ed8cae
jeison guzman <jeigux@gmail.com>
2026-06-20 12:21:10 -0500

    ajustes de cors

--- src/api/clienteAxios.js
+++ src/api/clienteAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Detecta si estamos en local o en producción
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://rifas-backend-kyec.onrender.com' // <--- PON AQUÍ TU URL DE RENDER
+  ? 'https://rifas-backend-kyec.onrender.com/api'
   : 'http://localhost:3000/api';
 
 const clienteAxios = axios.create({
